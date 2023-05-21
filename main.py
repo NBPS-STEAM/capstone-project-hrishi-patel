0472120f73290026793669efae777f6551dac7ad
theMetaHack <65082506+theMetaHack@users.noreply.github.com>
2023-05-21 11:50:22 -0400

    Update main.py
    shortened

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential, save_model, load_model
-from tensorflow.keras.layers import Dense, Flatten
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Load the MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# Preprocess the data
-x_train = x_train / 255.0
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train / 255.0,
 x_test = x_test / 255.0
 
-# Define the model
-model = Sequential([
-    Flatten(input_shape=(28, 28)),
-    Dense(128, activation='relu'),
-    Dense(10, activation='softmax')
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128,activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-# Compile the model
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5)
 
-# Train the model
-model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
-
-# Evaluate the model
-loss, accuracy = model.evaluate(x_test, y_test)
-print(f'Test loss: {loss}')
-print(f'Test accuracy: {accuracy}')
-
-# Save the model
-save_model(model, 'digits.model')
-print("Model saved as 'digits.model'.")
+model.save('digits.model')
