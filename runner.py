4a7815c2bb4fadc05cf2f9f00e9c66c3c60ca67f
theMetaHack <65082506+theMetaHack@users.noreply.github.com>
2023-05-21 11:51:02 -0400

    Update runner.py
    GUI implemented

--- runner.py
+++ runner.py
@@ -1,30 +1,19 @@
+import gradio
+import gradio as gr
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential, save_model, load_model
-from tensorflow.keras.layers import Dense, Flatten
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Load the model
-loaded_model = load_model('digits.model')
+new_model = tf.keras.models.load_model('digits.model')
 
-# Test on your own files
-# Here, you need to provide the path to your own image files
-image_files = [r"C:\Users\conta\Downloads\seven.png"]
 
-for image_file in image_files:
-    img = plt.imread(image_file)
-    img = img[:, :, 0]  # Convert RGB image to grayscale
+def classify(input):
+    input = np.reshape(input, (1, 28, 28))
+    prediction = new_model.predict(input).tolist()[0]
+    return {str(i): prediction[i] for i in range(10)}
 
-    # Preprocess the image
-    img = img / 255.0
-    img = np.reshape(img, (1, 28, 28))  # Reshape to match input shape of the model
+label = gr.outputs.Label(num_top_classes=10)
+sp = gradio.Sketchpad(image_mode="L", source="canvas", shape=(14, 14), invert_colors=False, interactive=True)
 
-    # Make prediction
-    prediction = loaded_model.predict(img)
-    predicted_digit = np.argmax(prediction)
-
-    plt.imshow(img[0], cmap='gray')
-    plt.title(f'Predicted Digit: {predicted_digit}')
-    plt.show()
-    print(predicted_digit)
+interface = gr.Interface(fn=classify, inputs="sketchpad", outputs=label,
+live=True)
+interface.launch()
